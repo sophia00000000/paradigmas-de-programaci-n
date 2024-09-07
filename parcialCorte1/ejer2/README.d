bab00e80ac8494639165b0f38597b2ae9405ed08
belen-exe <mbp.melendez06@gmail.com>
2024-09-06 20:03:03 -0500

    Update README.d

--- parcialCorte1/ejer2/README.d
+++ parcialCorte1/ejer2/README.d
@@ -1,1 +1,3 @@
-d
+La finalidad de estos programas es ordenar una lista de estudiantes y sus respectivas calificaciones, esto se desarrolla desde dos enfoques diferentes:
+En Haskell se usa el enfoque declarativo, se piensa en qué se logró usando solo las funciones de sortBy y comparing para ordenar la lista de estudiante, haciendolo recursivo y consumiendo menos lineas de codigo.
+En Python se usa el enfoque imperativo, ligado más al cómo se hizo y usando el metodo de comparacion de datos en el arreglo para ordenar la lista de estudiantes.
